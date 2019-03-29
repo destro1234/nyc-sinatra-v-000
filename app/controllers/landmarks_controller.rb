5e141f46dfb7104b1f35ce7de99415a252602043
Elias Ventura <eliasventura23@gmail.com>
2019-03-29 13:03:12 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,24 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmarks = Landmark.all
+    erb :'/landmarks/show'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    #@figure.titles << Title.create(:name=> params[:title][:name])
+    #@figure.landmarks << Landmark.create(:name=> params[:landmark][:name])
+    redirect "/landmarks"
+  end
 end
